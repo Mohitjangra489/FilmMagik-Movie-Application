9e2b259be58c69ae90d697cb59fa1c7f966b8cdb
Mohitjangra489 <119482895+Mohitjangra489@users.noreply.github.com>
2023-09-29 14:43:56 +0530

    some changes in css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
                 <button onClick={()=>{setisexpanded(!isexpanded)}} className='hamburger'>
                     <img src={image} className='img'></img>
                 </button>
-                <div className={isexpanded ? "navigation-menu.expanded":"navigation-menu"} >
+                <div className={isexpanded ? "navigation-menu expanded":"navigation-menu"} >
                     <ul>
                         <li><Link to={`/trending`}>Most Popular</Link></li>
                         <li><Link to={`/toprated`}>Top-Rated</Link></li>
@@ -26,13 +26,9 @@ function Navbar() {
                         <li><Link to={`/upcoming`}>New Releases</Link></li>
                         <li><Link to={`/searchmovies`}>SearchMovie</Link></li> */}
                         
-                        {/* <li>Horror</li>
-                        <li>Thriller</li>
-                        <li>series</li> */}
                     </ul>
                 </div>
                 </nav>
-                {/* <Outlet/> */}
                 </>
         
     )
